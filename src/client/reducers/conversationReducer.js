1d5f0d98725b126af8112aa7746c8b5bee248633
Tyler Lafayette <tylerlafayette@gmail.com>
2018-04-29 18:42:38 -0700

    Began redux integration + added decorator transform to webpack.config.js

---
+++ src/client/reducers/conversationReducer.js
@@ -0,0 +1,10 @@
+const reducer = (state={
+        conversations: [],
+        fetching: false,
+        fetched: false,
+        error: null,
+    }, action) => {
+    return {...state}
+}
+
+export default reducer
---
+++ src/client/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux"
+
+import conversations from "./conversationsReducer"
+
+export default combineReducers({
+    conversations
+})
