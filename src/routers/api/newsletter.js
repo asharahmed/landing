286d90e6417c870bb876452bd6913af0e05e9df5
Tyler Lafayette <tylerlafayette@gmail.com>
2018-09-10 16:04:55 -0700

    Switched to async

--- src/routers/api/newsletter.js
+++ src/routers/api/newsletter.js
@@ -7,31 +7,36 @@ client.setApiKey(process.env.SENDGRID_API_KEY)
 
 const list_id = "5038953"
 
-router.post("/submitEmail", (req, res) => {
-    if(!req.body.email) res.send(error.INVALID_DATA)
+router.post("/submitEmail", async (req, res) => {
     const email = req.body.email
-    console.log(email)
-    const createRecipient = {
-        method: "POST",
-        url: "/v3/contactdb/recipients",
-        body: [{
-            email
-        }]
+    if(!email) return res.send(error.INVALID_DATA)
+    let request
+    try{
+        request = await client.request({
+            method: "POST",
+            url: "/v3/contactdb/recipients",
+            body: [{
+                email
+            }]
+        })
+    }catch(error) {
+        return res.send(error.API_ERROR)
     }
-    client.request(createRecipient)
-        .then(([Response, body]) => {
-            if(body.error_count > 0 || !body.persisted_recipients)
-                return res.send({...error.API_ERROR, err: body.errors[0] || "API Error"})
-            const recipId = body.persisted_recipients[0]
-            let addToList = {
-                method: "POST",
-                url: `/v3/contactdb/lists/${list_id}/recipients/${recipId}`,
-            }
-            return client.request(addToList)
+    let [Response, body] = request
+    if(body.error_count > 0 || !body.persisted_recipients)
+        return res.send({...error.API_ERROR, err: body.errors[0] || "API Error"})
+    let recipId = body.persisted_recipients[0]
+    let url = "/v3/contactdb/lists/" + list_id + "/recipients/" + recipId
+    try
+    {
+        request = await client.request({
+            method: "POST",
+            url,
         })
-        .then(([Response, body]) => {
-            return res.send(success.NO_ERROR)
-        })
+    }catch(error) {
+        return res.send(error.API_ERROR)
+    } 
+    return res.send(success.NO_ERROR)
 })
 
 module.exports = router
